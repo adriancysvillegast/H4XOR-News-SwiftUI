60937efb44e29681cadee3d33b3f5cb7a3202535
Adriancys Jesus Villegas Toro <81894293+adriancysvillegast@users.noreply.github.com>
2021-12-30 12:59:53 -0400

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Adriancys Jesus Villegas Toro on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
